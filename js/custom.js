f127144ba8569e1c8deb81c090cf941134d49c76
Your Name <you@example.com>
2022-09-15 00:08:35 +0300

    11

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(document).ready(function () {
+ /*   $('.carousel').carousel({
+        interval: 4000
+    });
+	*/
+});
+/*
+$(".owl-carousel.content-slider-with-large-controls").owlCarousel({
+    singleItem: true,
+    autoPlay: false,
+    navigation: true,
+    pagination: true
+});
+
+$(".owl-carousel.content-slider").owlCarousel({
+    singleItem: true,
+    autoPlay: 5000,
+    navigation: false,
+    navigationText: false,
+    pagination: false
+});
+
+*/
+
+/*
+var iframe = document.getElementById('video');
+var player = $f(iframe);
+*/
+
+// bind events
+
+/*
+var playButton = document.getElementById("vid-play");
+playButton.addEventListener("click", function () {
+    player.api("play");
+});
+
+var pauseButton = document.getElementById("close");
+pauseButton.addEventListener("click", function () {
+    player.api("pause");
+});
+
+*/
+
+
+$("#video").contents().find('.sidedock').css('background-color', '#ffffff');
+$(".newYear").text((new Date).getFullYear());
